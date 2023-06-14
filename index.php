cc5014c24050820a7ef2f8fb78a75629f35834e4
DANY201067 <ramirezlimasdanny@gmail.com>
2023-06-14 12:20:45 -0500

    camobios en el login

--- index.php
+++ index.php
@@ -82,14 +82,14 @@
       <div class="d-flex justify-content-around mt-1">
         <div class="d-flex align-items-center gap-1">
           <input class="form-check-input" type="checkbox" />
-          <div class="pt-1" style="font-size: 0.9rem">Remember me</div>
+          <div class="pt-1" style="font-size: 0.9rem">Acuérdate de mí</div>
         </div>
         <div class="pt-1">
           <a
             href="#"
             class="text-decoration-none text-info fw-semibold fst-italic"
             style="font-size: 0.9rem"
-            >Forgot your password?</a
+            >¿Olvidaste tu contraseña?</a
           >
         </div>
       </div>
@@ -97,7 +97,7 @@
         Login
       </div>
       <div class="d-flex gap-1 justify-content-center mt-1">
-        <div>Don't have an account?</div>
+        <div>¿No tienes una cuenta?</div>
         <a href="#" class="text-decoration-none text-info fw-semibold"
           >Register</a
         >
